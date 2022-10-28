6f38ebbf3df960649c52fd3b6c5a8f41da482a94
GitHub Action <action@github.com>
2022-10-28 13:00:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/boatrainlsz/gituserplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/boatrainlsz/gituserplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.boatrainlsz.gituserplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boatrainlsz.gituserplugin.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/boatrainlsz/gituserplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/boatrainlsz/gituserplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.boatrainlsz.gituserplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
